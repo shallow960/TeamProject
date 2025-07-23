f7772972dcb2cf8af361e086bd2bfc46b164eb9b
Nullsec <ejaebok5252@gmail.com>
2025-07-23 11:28:23 +0900

    🐙save fundcontroller, fundrepository

--- Project/backend/src/main/java/com/project/fund/controller/FundController.java
+++ Project/backend/src/main/java/com/project/fund/controller/FundController.java
@@ -1,16 +1,22 @@
 package com.project.fund.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.fund.dto.request.FundItemAppRequestDto;
+import com.project.fund.dto.request.FundRegularSponAppRequestDto;
 import com.project.fund.dto.request.FundSponAppRequestDto;
+import com.project.fund.dto.response.FundItemAppResponseDto;
+import com.project.fund.dto.response.FundRegularSponAppResponseDto;
+import com.project.fund.dto.response.FundSponAppResponseDto;
+import com.project.fund.entity.FundEntity;
 import com.project.fund.service.FundService;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,17 +24,50 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/") //미정
 public class FundController {
 	
-	private final FundService fundEntity;
+	private final FundService fundService;
 	
+	//사용자가 (후원금 신청) 후원하기 버튼을 눌렀을 때
 	@PostMapping("/")
-	public ResponseEntity<?> saveFundSponApp(
-			@RequestBody @Valid FundSponAppRequestDto dto,
-			@AuthenticationPrincipal CustomUserDetails userDetails){
-		Long memberId = userDetails.getMemberId();
-		FundService.saveSponApp(dto, memberId);
-		
-		return ResponseEntity.ok("후원신청 완료")
+	public ResponseEntity<?> saveFundSponApp(@RequestBody FundSponAppRequestDto dto){
+		fundService.saveSponApp(dto);
+		return ResponseEntity.ok("후원 완료");
 	}
 	
+	//사용자가 (후원물품 신청) 후원하기 버튼 눌렀을 때
+	@PostMapping("/")
+	public ResponseEntity<?> saveFundItemApp(@RequestBody FundItemAppRequestDto dto){
+		fundService.saveItemApp(dto);
+		return ResponseEntity.ok("후원 완료");
+	}
+	
+	//사용자가 (정기후원 신청) 후원하기 버튼 눌렀을 때
+	@PostMapping("/")
+	public ResponseEntity<?> saveRegularSponApp(@RequestBody FundRegularSponAppRequestDto dto){
+		fundService.saveRegularSponApp(dto);
+		return ResponseEntity.ok("후원 완료");
+	}
+	
+	//회원 (후원금 신청) 후원하기 버튼 클릭 후 내역 조회
+	@PostMapping("/")
+	public ResponseEntity<?> getFundSponApp(@RequestBody FundSponAppResponseDto dto){
+		List<FundEntity> result = fundService.getFundListByGuest(dto.getFundSponsor(), dto.getFundPhone());
+		return ResponseEntity.ok(result);
+	}
+	
+	//사용자가 (후원물품 신청) 후원하기 버튼 클릭 후 내역 조회
+	@PostMapping("/")
+	public ResponseEntity<?> getFundItemApp(@RequestBody FundItemAppResponseDto dto){
+		List<FundEntity> result = fundService.getFundListByGuest(dto.getFundSponsor(),dto.getFundPhone());
+		return ResponseEntity.ok(result);
+				
+				
+	}
+	
+	//사용자가 (정기 후원) 후원하기 버튼 클릭 후 내역 조회
+	@PostMapping("/")
+	public ResponseEntity<?> getFundRegularSponApp(@RequestBody FundRegularSponAppResponseDto dto){
+		List<FundEntity> result = fundService.getFundListByGuest(dto.getFundSponsor(),dto.getFundPhone());
+		return ResponseEntity.ok(result);
+	}
 
 }
--- Project/backend/src/main/java/com/project/fund/repository/FundRepository.java
+++ Project/backend/src/main/java/com/project/fund/repository/FundRepository.java
@@ -16,5 +16,4 @@ public interface FundRepository extends JpaRepository<FundEntity, Integer>{
 
 
 	
-	
 }
