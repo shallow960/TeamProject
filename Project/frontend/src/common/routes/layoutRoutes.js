02bdd9bfb860912855f4aff8d1d312bdf30ba16d
shallow960 <dksgudwn45@gmail.com>
2025-08-21 11:24:37 +0900

    안형주 라우터 경로 수정

--- Project/frontend/src/common/routes/layoutRoutes.js
+++ Project/frontend/src/common/routes/layoutRoutes.js
@@ -1,14 +1,22 @@
 // 📁 src/router/layoutRoutes.js
-
+import React from "react";
 import { Route } from "react-router-dom";
 import routes from "./router";
+import { RequireUserOnMember } from "./RouteGuards";
 
-// 📌 각 페이지 컴포넌트 import
-import LoginPage from "../../program/login/pages/LoginPage";
-import LogoutLink from "../../program/login/pages/LogoutLink";
-import AdminLogin from "../../program/admin/pages/AdminLoginPage";
-import Signup from "../../program/signup/pages/SignupPage";
+// 📌 각 페이지 컴포넌트
+import LoginPage from "../../program/login/pages/LoginPage"; //로그인
+import LogoutLink from "../../program/login/pages/LogoutLink"; //로그아웃
+import Signup from "../../program/signup/pages/SignupPage"; //회원가입
+import Admin from "../../program/admin/pages/AdminPage"; //관리자 로그인시 출력
+import AdminPw from "../../program/admin/pages/AdminPasswordUpdatePage"; //관리자 비밀번호 변경
+import FindId from "../../program/member/pages/FindIdPage"; //아이디 찾기
+import FindPw from "../../program/member/pages/FindPasswordPage"; //비밀번호 찾기
+import ChangePw from "../../program/member/pages/UpdatePasswordPage"; //비밀번호 변경
+import MyPage from "../../program/member/pages/Mypage"; //마이페이지
+import MemberPage from "../../program/member/pages/MemberData"; //회원정보
 
+// 📌 layoutRoutes 정의
 
 // FundPage.js에서 기본 내보내기가 없으므로, FundMainPage를 이름으로 불러옵니다.
 // 이제 FundPage.js에서 내보내는 모든 컴포넌트를 불러옵니다.
@@ -19,56 +27,229 @@ import {
   GoodsApplicationDetails,
   GoodsApplicationForm,
   RecurringApplicationDetails,
-  RecurringApplicationForm
+  RecurringApplicationForm,
 } from "../../program/fund/services/FundPage";
 
-// 1:1 채팅 컴포넌트 import
-import ChatList from "../../program/chat/services/ChatList";
-import ChatRoom from "../../program/chat/services/ChatRoom";
+// 게시판 관련
+import Normal from "../../bbs/normalbbs/Normal";
+import NormalBbsView from "../../bbs/normalbbs/NormalBbsview";
+import NormalBbsWrite from "../../bbs/normalbbs/NormalBbsWrite";
+import NormalBbsEdit from "../../bbs/normalbbs/NormalBbsEdit";
+import MemberNormalBbs from "../../bbs/normalbbs/MemberNormalBbs";
+import MemberNormalBbsView from "../../bbs/normalbbs/MemberNormalBbsView";
+import MemberNormalBbsDummy from "../../bbs/normalbbs/MemberNormalBbsdummy";
+import AdminNormalBbsdummy from "../../bbs/normalbbs/AdminNormalBbsdummy";
 
-// 입양 신청서 컴포넌트 import
-import AdoptApplicationForm from "../../program/adopt/services/AdoptApplicationForm";
+import QnaBbs from "../../bbs/questionbbs/QnaBbs";
+import QnaBbsWrite from "../../bbs/questionbbs/QnaBbsWrite";
+import QnaBbsView from "../../bbs/questionbbs/QnaBbsView";
+import QnaBbsEdit from "../../bbs/questionbbs/QnaBbsEdit";
+import AdminQnaBbs from "../../bbs/questionbbs/AdminQnaBbs";
+import AdminQnaBbsView from "../../bbs/questionbbs/AdminQnaBbsView";
+import AdminQnaBbsDummy from "../../bbs/questionbbs/AdminQnaBbsDummy";
 
-// AnimalForm 컴포넌트 import
-import AnimalForm from "../../program/animal/services/AnimalForm";
-
-// mapdata 컴포넌트
-import MapForm from "../../program/mapdata/services/MapForm";
-import MapPage from "../../program/mapdata/services/MapPage";
+import ImgList from "../../bbs/imagebbs/imgList";
+import ImgDetail from "../../bbs/imagebbs/imgdetail";
+import ImgEdit from "../../bbs/imagebbs/imgedit";
+import ImgWrite from "../../bbs/imagebbs/imgwrite";
+import AdminImgBoard from "../../bbs/imagebbs/AdminImgBoard";
+import AdminImgDetail from "../../bbs/imagebbs/AdminImgDetail";
+import ImgBoardDummy from "../../bbs/imagebbs/ImgBoardDummy";
 
 // 📌 routes 객체 기반으로 Route 구성
 const layoutRoutes = [
-  <Route key="login" path={routes.member.login.path} element={<LoginPage />} />,
-  <Route key="login" path={routes.admin.login.path} element={<AdminLogin />} />,
-  <Route key="logout" path={routes.member.logout.path} element={<LogoutLink />} />,
-  <Route key="signup" path={routes.member.signup.path} element={<Signup />} />,
-  
+  // 로그인/회원 관련
+  <Route key="login" path={routes.member.login.path} element={<LoginPage />} />, //로그인
+  <Route
+    key="logout"
+    path={routes.member.logout.path}
+    element={<LogoutLink />}
+  />, //로그아웃
+  <Route key="signup" path={routes.member.signup.path} element={<Signup />} />, //회원가입
+  <Route
+    key="mypage"
+    path={routes.member.mypage.path}
+    element={
+      <RequireUserOnMember>
+        <MyPage />
+      </RequireUserOnMember>
+    }
+  />, //마이페이지
+  <Route
+    key="memberdata"
+    path={routes.member.memberdata.path}
+    element={
+      <RequireUserOnMember>
+        <MemberPage />
+      </RequireUserOnMember>
+    }
+  />, //회원정보
+  <Route key="find-id" path={routes.member.findid.path} element={<FindId />} />, //아이디 찾기
+  <Route key="find-pw" path={routes.member.findpw.path} element={<FindPw />} />, //비밀번호 찾기
+  <Route
+    key="update-password"
+    path={routes.member.changepw.path}
+    element={<ChangePw />}
+  />, //비밀번호 변경
 
+  //관리자
+  <Route key="admin" path={routes.admin.admin.path} element={<Admin />} />, //관리자 로그인시 출력
+  <Route
+    key="updatePw"
+    path={routes.admin.password.path}
+    element={
+      <RequireUserOnMember>
+        <AdminPw />
+      </RequireUserOnMember>
+    }
+  />, //관리자 비밀번호 변경
+
+  // 일반 게시판
+  <Route key="normal" path="/bbs/normal" element={<Normal />} />,
+  <Route
+    key="normal-bbs-view"
+    path="/bbs/normal/:id"
+    element={<NormalBbsView />}
+  />,
+  <Route
+    key="normal-bbs-write"
+    path="/bbs/normal/write"
+    element={<NormalBbsWrite />}
+  />,
+  <Route
+    key="normal-bbs-edit"
+    path="/bbs/normal/edit"
+    element={<NormalBbsEdit />}
+  />,
+  <Route
+    key="Membernormal"
+    path="/bbs/member/normal"
+    element={<MemberNormalBbs />}
+  />,
+  <Route
+    key="Membernormal-view"
+    path="/bbs/member/normal/view"
+    element={<MemberNormalBbsView />}
+  />,
+  <Route
+    key="Membernormal-dummy"
+    path="/bbs/member/normal/dummy"
+    element={<MemberNormalBbsDummy />}
+  />,
+  <Route
+    key="Adminnormal-dummy"
+    path="/bbs/Admin/normal/dummy"
+    element={<AdminNormalBbsdummy />}
+  />,
+
+  // QnA 게시판
+  <Route key="qna-bbs" path="/bbs/qna" element={<QnaBbs />} />,
+  <Route key="qna-bbs-write" path="/bbs/qna/write" element={<QnaBbsWrite />} />,
+  <Route key="qna-bbs-view" path="/bbs/qna/:id" element={<QnaBbsView />} />,
+  <Route key="qna-bbs-edit" path="/bbs/qna/edit" element={<QnaBbsEdit />} />,
+  <Route key="qna-bbs-admin" path="/bbs/admin/qna" element={<AdminQnaBbs />} />,
+  <Route
+    key="qna-bbs-admin-view"
+    path="/bbs/admin/qna/view"
+    element={<AdminQnaBbsView />}
+  />,
+  <Route
+    key="qna-bbs-admin-dummy"
+    path="/bbs/admin/qna/dummy"
+    element={<AdminQnaBbsDummy />}
+  />,
+
+  // 이미지 게시판
+  <Route key="img-list" path="/bbs/image" element={<ImgList />} />,
+  <Route key="img-detail" path="/bbs/image/:id" element={<ImgDetail />} />,
+  <Route key="img-edit" path="/bbs/image/:id/edit" element={<ImgEdit />} />,
+  <Route key="img-write" path="/bbs/image/write" element={<ImgWrite />} />,
+  <Route
+    key="img-Admin-ImgBoard"
+    path="/bbs/admin/image"
+    element={<AdminImgBoard />}
+  />,
+  <Route
+    key="img-Admin-ImgDetail"
+    path="/bbs/admin/image/Detail"
+    element={<AdminImgDetail />}
+  />,
+  <Route
+    key="img-Admin-ImgDummy"
+    path="/bbs/admin/image/Dummy"
+    element={<ImgBoardDummy />}
+  />,
 
   // 후원 route
   // 각 후원 페이지를 별도의 경로로 라우팅합니다.
-  <Route key="fundMainPage" path={routes.fund.root.path} element={<FundMainPage />} />,
-  <Route key="fundForm" path={routes.fund.fundForm.path} element={<FundApplicationForm />} />,
-  <Route key="goodsForm" path={routes.fund.goodsForm.path} element={<GoodsApplicationForm />} />,
-  <Route key="recurringForm" path={routes.fund.recurringForm.path} element={<RecurringApplicationForm />} />,
-  <Route key="fundDetails" path={routes.fund.fundDetails.path} element={<FundApplicationDetails />} />,
-  <Route key="goodsDetails" path={routes.fund.goodsDetails.path} element={<GoodsApplicationDetails />} />,
-  <Route key="recurringDetails" path={routes.fund.recurringDetails.path} element={<RecurringApplicationDetails />} />,
+  <Route
+    key="fundMainPage"
+    path={routes.fund.root.path}
+    element={<FundMainPage />}
+  />,
+  <Route
+    key="fundForm"
+    path={routes.fund.fundForm.path}
+    element={<FundApplicationForm />}
+  />,
+  <Route
+    key="goodsForm"
+    path={routes.fund.goodsForm.path}
+    element={<GoodsApplicationForm />}
+  />,
+  <Route
+    key="recurringForm"
+    path={routes.fund.recurringForm.path}
+    element={<RecurringApplicationForm />}
+  />,
+  <Route
+    key="fundDetails"
+    path={routes.fund.fundDetails.path}
+    element={<FundApplicationDetails />}
+  />,
+  <Route
+    key="goodsDetails"
+    path={routes.fund.goodsDetails.path}
+    element={<GoodsApplicationDetails />}
+  />,
+  <Route
+    key="recurringDetails"
+    path={routes.fund.recurringDetails.path}
+    element={<RecurringApplicationDetails />}
+  />,
 
   // 1:1 채팅 route 추가
-  <Route key="chatList" path={routes.admin.chat.list.path} element={<ChatList />} />,
-  <Route key="chatRoom" path={routes.admin.chat.room(':roomId')} element={<ChatRoom />} />,
+  <Route
+    key="chatList"
+    path={routes.admin.chat.list.path}
+    element={<ChatList />}
+  />,
+  <Route
+    key="chatRoom"
+    path={routes.admin.chat.room(":roomId")}
+    element={<ChatRoom />}
+  />,
 
   // 입양 신청서 route 추가
-    <Route key="adoptForm" path={routes.admin.adopt.form(':memberNum')} element={<AdoptApplicationForm />} />,
+  <Route
+    key="adoptForm"
+    path={routes.admin.adopt.form(":memberNum")}
+    element={<AdoptApplicationForm />}
+  />,
   // 동물 정보 기입 route 추가
-    <Route key="animalRegister" path={routes.admin.animal.register.path} element={<AnimalForm />} />,
+  <Route
+    key="animalRegister"
+    path={routes.admin.animal.register.path}
+    element={<AnimalForm />}
+  />,
 
   // 맵 관련 route 추가
-    <Route key="mapPage" path={routes.mapdata.map.path} element={<MapPage />} />,
-    <Route key="mapRegister" path={routes.mapdata.register.path} element={<MapForm />} />,
-
-    
+  <Route key="mapPage" path={routes.mapdata.map.path} element={<MapPage />} />,
+  <Route
+    key="mapRegister"
+    path={routes.mapdata.register.path}
+    element={<MapForm />}
+  />,
 ];
 
 export default layoutRoutes;
--- Project/frontend/src/common/routes/router.js
+++ Project/frontend/src/common/routes/router.js
@@ -24,20 +24,28 @@ const routes = {
     reserve: { path: "/land/reserve", label: "예약하기" },
   },
 
+  // 게시판
   board: {
-    root: { path: "/board", label: "게시판" },
-    notice: { path: "/board/notice", label: "공지사항" },
-    qna: { path: "/board/qna", label: "질문 게시판" },
-    photo: { path: "/board/photo", label: "포토 게시판" },
+    root: { path: "/bbs", label: "게시판" },
+    notice: { path: "/bbs/normalbbs", label: "공지사항" },
+    qna: { path: "/bbs/questionbbs", label: "질문 게시판" },
+    photo: { path: "/bbs/imagebbs", label: "포토 게시판" },
   },
 
   member: {
     signup: { path: "/signup", label: "회원가입" },
     login: { path: "/login", label: "로그인" },
     logout: { path: "/logout", label: "로그아웃" },
+    findid: { path: "/find-id", label: "아이디 찾기" },
+    findpw: { path: "/find-pw", label: "비밀번호 찾기" },
     mypage: { path: "/mypage", label: "마이페이지" },
-    update: { path: "/mypage/update", label: "회원정보 수정" },
-    delete: { path: "/mypage/update/delete", label: "회원 탈퇴" },
+    changepw: { path: "/member/update-password", label: "비밀번호 변경" },
+    memberdata: { path: "/member/mypage/memberdata", label: "회원정보" },
+    update: {
+      path: "/member/mypage/memberdata/update",
+      label: "회원정보 수정",
+    },
+    delete: { path: "/member/mypage/update/delete", label: "회원 탈퇴" },
   },
   // 후원 경로
   fund: {
@@ -47,13 +55,17 @@ const routes = {
     recurringForm: { path: "/fund/recurring", label: "정기후원 신청" },
     fundDetails: { path: "/fund/money-details", label: "후원금 상세" },
     goodsDetails: { path: "/fund/goods-details", label: "후원물품 상세" },
-    recurringDetails: { path: "/fund/recurring-details", label: "정기후원 상세" },
+    recurringDetails: {
+      path: "/fund/recurring-details",
+      label: "정기후원 상세",
+    },
   },
 
   admin: {
     login: { path: "/admin/login", label: "관리자 로그인" },
-    dashboard: { path: "/admin/dashboard", label: "관리자 홈" },
-    members: { path: "/admin/members", label: "회원 관리" },
+    dashboard: { path: "/admin", label: "관리자 홈" },
+    password: { path: "/admin/updatePw", label: "관리자 비밀번호 변경" }, //안형주 추가
+    members: { path: "/admin/membersList", label: "회원 관리" },
     memberDetail: (id) => `/admin/members/${id}`,
     // 1:1 채팅 경로 추가
     chat: {
@@ -61,20 +73,19 @@ const routes = {
       room: (id) => `/admin/chat/room/${id}`,
     },
     // 입양 신청서 경로 추가
-      adopt: {
-          form: (memberNum) => `/admin/adopt/request/${memberNum}`,
-          label: "입양 신청서"
-      },
+    adopt: {
+      form: (memberNum) => `/admin/adopt/request/${memberNum}`,
+      label: "입양 신청서",
+    },
     // 동물 관리 경로 추가
-      animal: {
-          register: { path: "/admin/animal/register", label: "동물 정보 기입" },
-      },
+    animal: {
+      register: { path: "/admin/animal/register", label: "동물 정보 기입" },
+    },
   },
 
   // frontend/src/common/routes/router.js
 
-
-  mapdata:{
+  mapdata: {
     root: { path: "/map", label: "동물 놀이터" },
     info: { path: "/map/info", label: "놀이터 소개" },
     gallery: { path: "/map/gallery", label: "놀이터 둘러보기" },
@@ -83,7 +94,6 @@ const routes = {
     map: { path: "/map", label: "놀이터 위치" },
     register: { path: "/map/map/register", label: "장소 등록" },
   },
-
 
   reservation: {
     list: { path: "/reservation/list", label: "예약 목록" },
